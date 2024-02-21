7a83a59c35f80bf9b7a11cfc4ee753d8b6c1d573
archieewald <arkadiuszdabek@gmail.com>
2024-02-21 17:04:50 +0100

    feat(auth): add Google auth and setup nodemon

--- index.ts
+++ index.ts
@@ -1,11 +1,34 @@
 import express from 'express';
+import passport from 'passport';
+import GooglePassport from 'passport-google-oauth20';
+
+const GoogleStrategy = GooglePassport.Strategy;
+
 const app = express();
 
-const PORT = process.env.PORT;
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID || '',
+      clientSecret: process.env.CLIENT_KEY || '',
+      callbackURL: process.env.CALLBACK_URL
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log({ accessToken, refreshToken, profile });
+    }
+  )
+);
 
-app.get('/', (_req, res) => {
-  res.send({ nobody: 'Hello World!' });
-});
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+const PORT = process.env.PORT;
 
 app.listen(PORT);
 
